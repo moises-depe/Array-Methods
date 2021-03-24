efd536a5de1696a24eafec5ea33b63305faadba8
moises depena <swe.moisesdepe@gmail.com>
2021-03-24 10:13:53 -0700

    everyFinal

---
+++ every/every.js
@@ -0,0 +1,8 @@
+const every = (array, callback) => {
+    for (let i = 0; i < array.length; i++) {
+      if (!callback(array[i])) return false;
+    }
+    return true;
+  };
+
+  module.exports = every;
